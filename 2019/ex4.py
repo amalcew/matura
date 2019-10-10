dc01205bc72cd9ef9bec014426c1878f94f576e9
amalcew <amalcew@tutanota.com>
2019-10-10 21:52:39 +0200

    2019/ex4 completed

--- 2019/ex4.py
+++ 2019/ex4.py
@@ -1,13 +1,4 @@
-def test():
-    flag = True
-    while flag:
-        for x in range(0, 5000):
-            number = 3 ** x
-            if number < 100000:
-                print(number)
-            else:
-                flag = False
-
+from math import gcd
 
 def multiply(n):
     total = 1
@@ -19,7 +10,7 @@ def multiply(n):
 def ex4_1():
     how_many = 0
     power = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    numbers = open("Dane_PR2/liczby.txt", "r")
+    numbers = open("data/liczby.txt", "r")
     for x in range(0, 500):
         number = numbers.readline()
         number = int(number)
@@ -33,7 +24,7 @@ def ex4_1():
 
 
 def ex4_2():
-    numbers = open("Dane_PR2/liczby.txt", "r")
+    numbers = open("data/liczby.txt", "r")
     for x in range(0, 500):
         number = numbers.readline()
         check = int(number)
@@ -55,7 +46,46 @@ def ex4_2():
 
 
 def ex4_3():
-    pass
+    data = open("data/liczby.txt", "r")
+    numbers = []
+    # convert file to list of ints
+    for i in range(0, 500):
+        numbers.append(int(data.readline()))
+
+    series = []
+    tmp_series = [numbers[0]]
+    # main loop
+    for x in range(1, 500):
+        tmp_series.append(numbers[x])
+        # check GCD for series of numbers
+        check = tmp_series[0]
+        for y in tmp_series[1:]:
+            check = gcd(check, y)
+
+        if check == 1:
+            # delete invalid number
+            tmp_series.pop()
+            if len(tmp_series) > len(series):
+                series.clear()
+                # convert tmp_series to series
+                for z in range(len(tmp_series)):
+                    series.append(tmp_series[z])
+            # clear temporary list
+            tmp_series.clear()
+            # append last number and invalid number
+            tmp_series.append(numbers[x-1])
+            tmp_series.append(numbers[x])
+    # check GCD for the series
+    max_divisor = series[0]
+    for j in series[1:]:
+        max_divisor = gcd(max_divisor, j)
+    # answer
+    print("Pierwsza  liczba: "+str(series[0]))
+    print("Długość: "+str(len(series)))
+    print("NWD: "+str(max_divisor))
+    data.close()
 
 
+ex4_1()
 ex4_2()
+ex4_3()
