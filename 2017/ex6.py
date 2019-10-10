3a59a2b45f3b33aec0816d05bc4a3210519e737d
amalcew <amalcew@tutanota.com>
2019-10-10 10:46:15 +0200

    loop skeleton completed

--- 2017/ex6.py
+++ 2017/ex6.py
@@ -46,178 +46,29 @@ def ex6_3():
     print("---------")
     for y in range(0, 200):
         for x in range(0, 320):
+            pixel = image[y][x]
+            # top edge
             if y == 0:
+                # left top corner
                 if x == 0:
-                    # left top corner
-                    check1 = abs(image[y][x] - image[y][x+1])
-                    check2 = abs(image[y][x] - image[y+1][x])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                elif x == 319:
-                    # right top corner
-                    check1 = abs(image[y][x] - image[y][x-1])
-                    check2 = abs(image[y][x] - image[y+1][x])
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
                 else:
-                    # top edge
-                    check1 = abs(image[y][x] - image[y][x+1])
-                    check2 = abs(image[y][x] - image[y+1][x])
-                    check3 = abs(image[y][x] - image[y][x-1])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check3 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-            if y == 199:
-                if x == 0:
-                    # left bottom corner
-                    check1 = abs(image[y][x] - image[y][x+1])
-                    check2 = abs(image[y][x] - image[y-1][x])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                elif x == 319:
-                    # right bottom corner
-                    check1 = abs(image[y][x] - image[y][x-1])
-                    check2 = abs(image[y][x] - image[y-1][x])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                else:
-                    # bottom edge
-                    check1 = abs(image[y][x] - image[y][x+1])
-                    check2 = abs(image[y][x] - image[y][x-1])
-                    check3 = abs(image[y][x] - image[y-1][x])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check3 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
+            # left edge
             if x == 0:
-                if y != 0 and y != 199:
-                    # left edge
-                    check1 = abs(image[y][x] - image[y][x+1])
-                    check2 = abs(image[y][x] - image[y+1][x])
-                    check3 = abs(image[y][x] - image[y][x-1])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check3 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-            if x == 319:
-                if y != 0 and y != 199:
-                    # right edge
-                    check1 = abs(image[y][x] - image[y+1][x])
-                    check2 = abs(image[y][x] - image[y][x-1])
-                    check3 = abs(image[y][x] - image[y-1][x])
-                    if check1 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check2 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
-                    if check3 > condition:
-                        print("x="+str(x+1)+" y="+str(y+1))
-                        print(image[y][x])
-                        print("---------")
-                        result += 1
+                # left bottom corner
+                if y == 319:
+                else:
+            # bottom edge
+            if y == 319:
+                # right bottom corner
+                if x == 199:
+                else:
+            # right edge
+            if x == 199:
+                # right top corner
+                if y == 0:
+                else:
+            # inner pixel
             else:
-                if y != 0 and y != 199:
-                    if x != 0 and x != 319:
-                        flag = False
-                        check1 = abs(image[y][x] - image[y][x+1])
-                        check2 = abs(image[y][x] - image[y+1][x])
-                        check3 = abs(image[y][x] - image[y][x-1])
-                        check4 = abs(image[y][x] - image[y-1][x])
-                        if check1 > condition:
-                            result += 1
-                            if image[y][x] > image[y][x+1]:
-                                flag = True
-                        if check2 > condition:
-                            result += 1
-                            if image[y][x] > image[y+1][x]:
-                                flag = True
-                        if check3 > condition:
-                            result += 1
-                            if image[y][x] > image[y][x-1]:
-                                flag = True
-                        if check4 > condition:
-                            result += 1
-                            if image[y][x] > image[y-1][x]:
-                                flag = True
-                        if flag:
-                            print("x=" + str(x + 1) + " y=" + str(y + 1))
-                            print(image[y][x])
-                            print("---------")
-                            result -= 4
-                            result += 1
-
-    print(result)
 
 
 
