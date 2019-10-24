96fc66b965d1835f326ef25ff6eb807d9026ff1b
amalcew <amalcew@tutanota.com>
2019-10-24 23:27:14 +0200

    exercises/ex66 added

---
+++ exercises/ex66.py
@@ -0,0 +1,54 @@
+# 457 126
+file = open("data/66/trojki.txt")
+numbers_1 = []
+numbers_2 = []
+numbers_3 = []
+
+for a in range(0,1000):
+    line = file.readline()
+    tmp = line.split()
+    numbers_1.append(tmp[0])
+    numbers_2.append(tmp[1])
+    numbers_3.append(tmp[2])
+
+
+def is_prime(number):
+    num = number
+    if num > 1:
+        for i in range(2, num // 2):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
+
+
+def ex66_1():
+    answer = []
+    for x in range(0, 1000):
+        tmp = []
+        check = 0
+        for y in range(len(numbers_1[x])):
+            tmp.append(int(numbers_1[x][y]))
+        for z in range(len(numbers_2[x])):
+            tmp.append(int(numbers_2[x][z]))
+        for i in range(len(tmp)):
+            check += tmp[i]
+        if check == int(numbers_3[x]):
+            answer.append(numbers_1[x]+" "+numbers_2[x]+" "+numbers_3[x])
+    print(answer)
+
+
+def ex66_2():
+    answer = []
+    for x in range(0, 1000):
+        test1 = is_prime(int(numbers_1[x]))
+        test2 = is_prime(int(numbers_2[x]))
+        test3 = int(numbers_1[x]) * int(numbers_2[x])
+        if int(test1):
+            if int(test2):
+                if int(test3) == int(numbers_3[x]):
+                    answer.append(numbers_1[x]+" "+numbers_2[x]+" "+numbers_3[x])
+    print(answer)
+
+
+ex66_2()
